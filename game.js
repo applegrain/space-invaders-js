560d3fe03f0d61a3f91e2049ab16e80b6e4906de
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-17 11:18:34 -0600

    add movement to player smearing, does not get cleaned up afterwards

--- game.js
+++ game.js
@@ -21,10 +21,12 @@
 
   Game.prototype = {
     update: function() {
-
+      for (var i = 0; i <this.bodies.length; i++) {
+        this.bodies[i].update();
+      }
     },
     draw: function(screen, gameSize) {
-      for (var i =0; i < this.bodies.length; i++) {
+      for (var i = 0; i < this.bodies.length; i++) {
         drawRect(screen, this.bodies[i]);
       }
     }
@@ -33,12 +35,18 @@
   var Player = function(game, gameSize) {
     this.game = game;
     this.size = { x: 15, y: 15 };
-    this.center = { x: gameSize.x /2, y: gameSize.y - this.size.x}
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x}
+    this.keyboarder = new Keyboarder();
+
   };
 
   Player.prototype = {
     update: function() {
-
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+      }
     }
   };
 
@@ -46,6 +54,24 @@
     screen.fillRect(body.center.x - body.size.x / 2,
                     body.center.y - body.size.y / 2,
                     body.size.x, body.size.y);
+  };
+
+  var Keyboarder = function() {
+    var keyState = {};
+
+    window.onkeydown = function(e) {
+      keyState[e.keyCode] = true;
+    };
+
+    window.onkeyup = function(e) {
+      keyState[e.keyCode] = false;
+    };
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
   };
 
   window.onload = function() {
